44b53681a978d454c6244d4076f5b4be0472f9e9
Jefferson170713 <jefferson.almeia05@aluno.ifce.edu.br>
2025-07-17 16:36:53 -0300

    main - Teste com a Rayssinha

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function HomePageTeste() {
     return (
-        <h1><strong>Rayssinha</strong> Meu anjo conteúdo especial para você! Te amo!</h1>
+        <h1>Rayssinha Meu anjo conteúdo especial para você! Te amo!</h1>
     )
 }
 
