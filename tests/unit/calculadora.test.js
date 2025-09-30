b1b0e5fc760a451b87e50c6533a602c030b084b8
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2025-09-30 15:11:55 +0000

    test: Add teste de status

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,15 +1,13 @@
 const calculadora = require("../../models/calculadora.js");
 
 test("Deve somar 100 + 600 e retornar 700", () => {
-    const result = calculadora.somar(100, 600);
-    console.log(result);
-    expect(result).toBe(700); 
-
+  const result = calculadora.somar(100, 600);
+  console.log(result);
+  expect(result).toBe(700);
 });
 
 test("O programa não deve retornar String, mas sim erro.", () => {
-    const result = calculadora.somar('jefferson', 17);
-    console.log(result);
-    expect(result).toBe("Erro: Argumentos inválidos"); 
-
+  const result = calculadora.somar("jefferson", 17);
+  console.log(result);
+  expect(result).toBe("Erro: Argumentos inválidos");
 });
