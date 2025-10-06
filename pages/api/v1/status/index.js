0a3f929ef6e297046601abd341f7e51d66c940af
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2025-10-06 19:11:20 +0000

    feat(infra): adiciona database.js para conexão com o banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
+import database from "../../../../infra/database.js";
+
 function status(requerest, response) {
+  console.log(database);
   response.status(200).json({ chave: "Meu status está ok!" });
 }
 
