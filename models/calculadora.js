f8a7584fe6fa29ba1df19347869e7fe7b4c5c884
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2025-10-09 17:55:32 +0000

    test(calculadora.js): Implementação das funçoes básicas(subtrair, dividir, multiplicar)

--- models/calculadora.js
+++ models/calculadora.js
@@ -5,4 +5,39 @@ function somar(numero1, numero2) {
   return numero1 + numero2;
 }
 
-exports.somar = somar;
+function subtrair(numero1, numero2) {
+  if (typeof numero1 !== "number" || typeof numero2 !== "number") {
+    return "Erro: Argumentos inválidos";
+  }
+  return numero1 - numero2;
+}
+
+function dividir(numero1, numero2) {
+  if (typeof numero1 !== "number" || typeof numero2 !== "number") {
+    return "Erro: Argumentos inválidos";
+  }
+  if (numero2 === 0) {
+    return "Erro: Não é possível dividir por zero";
+  }
+  return numero1 / numero2;
+}
+
+function multiplicar(numero1, numero2) {
+  if (typeof numero1 !== "number" || typeof numero2 !== "number") {
+    return "Erro: Argumentos inválidos";
+  }
+  return numero1 * numero2;
+};
+// exports.somar = somar;
+// exports.subtrair = subtrair;
+// exports.dividir = dividir;
+
+module.exports = { 
+  somar, 
+  subtrair, 
+  dividir, 
+  multiplicar
+};
+// export { somar, subtrair, dividir };
+// export default { somar, subtrair, dividir };
+
--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -2,12 +2,54 @@ const calculadora = require("../../models/calculadora.js");
 
 test("Deve somar 100 + 600 e retornar 700", () => {
   const result = calculadora.somar(100, 600);
-  console.log(result);
+  // console.log(result);
   expect(result).toBe(700);
 });
 
 test("O programa não deve retornar String, mas sim erro.", () => {
   const result = calculadora.somar("jefferson", 17);
-  console.log(result);
+  // console.log(result);
   expect(result).toBe("Erro: Argumentos inválidos");
+});
+
+test(" O programa não deve retornar String, mas sim um erro.", () => {
+  const result = calculadora.somar(17, "jefferson");
+  expect(result).toBe("Erro: Argumentos inválidos");
+});
+
+test("Deve subtrair 10 - 5 e retornar 5", () => {
+  const result = calculadora.subtrair(10, 5);
+  expect(result).toBe(5);
+});
+
+test("O programa não deve retornar String, mas sim erro.", () => {
+  const result = calculadora.subtrair("jefferson", 5);
+  expect(result).toBe("Erro: Argumentos inválidos");
+});
+
+test("O programa não deve retornar String, mas sim erro.", () => {
+  const result = calculadora.subtrair(10, "jefferson");
+  expect(result).toBe("Erro: Argumentos inválidos");
+});
+
+test("O programa não deve retornar String, mas sim erro.", () => {
+  const result = calculadora.multiplicar(10, "jefferson");
+  expect(result).toBe("Erro: Argumentos inválidos");
+});
+
+test("O programa não deve retornar String, mas sim erro.", () => {
+  const result = calculadora.multiplicar(10, 100);
+  expect(result).toBe(1000);
+});
+
+test("O programa não deve retornar String, mas sim erro.", () => {
+  const result = calculadora.multiplicar(10, 0);
+  // console.log(result);
+  expect(result).toBe(0);
+});
+
+test("O programa não deve retornar String, mas sim erro.", () => {
+  const result = calculadora.multiplicar(10, -1);
+  // console.log(result);
+  expect(result).toBe(-10);
 });
