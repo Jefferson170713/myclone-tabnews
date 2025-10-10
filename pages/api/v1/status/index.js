97d451cc531b4221b1295abcd3e990ac517a7d47
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2025-10-10 13:33:58 +0000

    test(endpoint:max_conections): Teste do número máximo de conecções.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,13 +5,18 @@ async function status(requerest, response) {
   const updateAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("Show server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;;
 
+  const databaseConectionsResult = await database.query("SHOW max_connections;");
+  const databaseConectionsValue = databaseConectionsResult.rows[0].max_connections;
+  const databaseConectionValuePareInt = parseInt(databaseConectionsValue);
+  
   response.status(200).json({
     updated_at: updateAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections : databaseConectionValuePareInt,
       }
     }
   });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,6 +11,7 @@ test("GET to /api/v1/status should return 200", async () => {
     expect(parseUpdatedAt).toEqual(responseBody.updated_at);
 
     expect(responseBody.dependencies.database.version).toEqual('16.0');
+    expect(responseBody.dependencies.database.max_connections).toBeDefined();
     // console.log(responseBody.dependencies.database.version);
     
 });
