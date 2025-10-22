85484a51a59945a5279aa7e438d085ce2332cf54
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2025-10-22 20:04:34 +0000

    refacto(get/post:test): Fazendo o jest entender o ambiente de test do jest com a configuração do next

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,4 +1,16 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: ".env.development",
+});
+
 import database from "infra/database.js"
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,17 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: ".env.development",
+});
+
+import database from "infra/database.js"
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
